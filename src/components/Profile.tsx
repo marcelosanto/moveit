ca78e7e22892fd7a151900b2180035047f67624f
Marcelo Santos <rocha.mer21@gmail.com>
2021-02-28 14:50:07 -0300

    Component  Profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import styles from '../styles/components/Profile.module.css'
+
+export function Profile() {
+  return (
+    <div className={styles.profileContainer}>
+      <img
+        src='https://avatars.githubusercontent.com/u/11478538?s=460&u=9c988c89e270ba12a7464b769297420abfcece2a&v=4'
+        alt='Marcelo Santos'
+      />
+      <div>
+        <strong>Marcelo Santos</strong>
+        <p>
+          <img src='icons/level.sgv' alt='' />
+          Level 1
+        </p>
+      </div>
+    </div>
+  )
+}
